f1286c10b6ae308351185ecf0cbfc26df86e48c7
Admin <liliya89175434888@gmail.com>
2024-11-26 11:27:34 +0300

    Создано окно с кноркой загрузить по тегу

--- Cats.py
+++ Cats.py
@@ -1,42 +1,58 @@
 from tkinter import *
+from tkinter import ttk
 from PIL import Image, ImageTk
 import requests
 from io import BytesIO
 
+# Список доступных тегов
+ALLOWED_TAGS = ['sleep', 'jump', 'smile', 'fight', 'black', 'white', 'red', 'siamese', 'bengal']
+
 def load_image(url):
     try:
-        # Отправляем GET-запрос с использованием requests.get()
         response = requests.get(url)
-
-        # Проверяем успешность запроса (код ответа 200)
         response.raise_for_status()
-
-        # Читаем байты из ответа в объект BytesIO
         image_data = BytesIO(response.content)
-
-        # Открываем изображение с помощью PIL
         img = Image.open(image_data)
-
+        img.thumbnail((600, 480), Image.Resampling.LANCZOS)
         return ImageTk.PhotoImage(img)
     except Exception as e:
         print(f"Ошибка при загрузке изображения: {e}")
         return None
 
+def open_new_window():
+    tag = tag_combobox.get()
+    url_with_tag = f'https://cataas.com/cat/{tag}' if tag else 'https://cataas.com/cat'
+    img = load_image(url_with_tag)
+    if img:
+        new_window = Toplevel()
+        new_window.title("Cat Image")
+        new_window.geometry("600x480")
+        label = Label(new_window, image=img)
+        label.image = img
+        label.pack()
+
 window = Tk()
 window.title("Cats!")
-window.geometry("600x480")
+window.geometry("600x520")
 
-# Создаем метку без изображения
-label = Label()
-label.pack()
+menu_bar = Menu(window)
+window.config(menu=menu_bar)
 
-url = 'https://cataas.com/cat'
-img = load_image(url)
+file_menu = Menu(menu_bar, tearoff=0)
+menu_bar.add_cascade(label="Файл", menu=file_menu)
+file_menu.add_command(label="Загрузить фото", command=open_new_window)
+file_menu.add_separator()
+file_menu.add_command(label="Выход", command=window.destroy)
 
-if img:
-    # Устанавливаем изображение в метку
-    label.config(image=img)
-    # Необходимо сохранить ссылку на изображение, чтобы избежать сборки мусора
-    label.image = img
+# Метка "Выбери тег"
+tag_label = Label(text="Выбери тег")
+tag_label.pack()
+
+tag_combobox = ttk.Combobox(values=ALLOWED_TAGS)
+tag_combobox.pack()
+
+# Кнопка "Загрузить по тегу"
+load_button = Button(window, text="Загрузить по тегу", command=open_new_window)
+load_button.pack(pady=10)
 
 window.mainloop()
